a8c78d94f9bbe21cfefe9da98b690c89b2dedaa1
botanki <karzan@botani.nu>
2018-04-11 13:20:57 +0200

    new contract address

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "450d3f737aa0e360a490"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "fd9cf1fac5d207b6105d"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","4":"bundles/pages/index.js","5":"bundles/pages/posts/allPosts.js"}[chunkId]||chunkId) + "-" + {"0":"297873ec6560f643ddee","1":"947300d43f3fd9b60597","2":"112a70b41bfbcd7c6dde","4":"3e8c7c03c2d6159e9c92","5":"140f7bb45db3c841cc10"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","4":"bundles/pages/index.js","5":"bundles/pages/posts/show.js"}[chunkId]||chunkId) + "-" + {"0":"297873ec6560f643ddee","1":"947300d43f3fd9b60597","2":"112a70b41bfbcd7c6dde","4":"8e7a6e914fdf6c1aa643","5":"8848e814ff04bd95b411"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
